93baa189ee7294e7e28b1d04dc2bb667c196fb95
robertof <nececuentag5tobiosep@gmail.com>
2022-02-08 17:28:15 -0600

    users_signup

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # Python
+import json
 from typing import Optional, List
 from uuid import UUID
 from datetime import date
@@ -12,6 +13,7 @@ from pydantic import Field
 # FastAPI
 from fastapi import FastAPI
 from fastapi import status
+from fastapi import Body
 
 app = FastAPI()
 
@@ -71,7 +73,7 @@ class Tweet(BaseModel):
     summary="Register a user",
     tags=["Users"]
 )
-def signup():
+def signup(user: UserRegister = Body(...)):
     """
     Signup
 
@@ -86,9 +88,17 @@ def signup():
     - **email: EmailStr**
     - **first_name: str**
     - **last_name: str**
-    - **birth_date: str**
-
+    - **birth_date: date**
     """
+    with open("users.json", "r+", encoding="utf-8") as f:
+        results = json.loads(f.read())
+        user_dict = user.dict()
+        user_dict["user_id"] = str(user_dict["user_id"])
+        user_dict["birth_date"] = str(user_dict["birth_date"])
+        results.append(user_dict)
+        f.seek(0)
+        f.write(json.dumps(results))
+        return user
 
 ### Login a user
 @app.post(
