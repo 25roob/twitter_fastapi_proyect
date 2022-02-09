cd7c5fbe11a51d2e32bb3ade4df51eb199ad2aa7
robertof <nececuentag5tobiosep@gmail.com>
2022-02-09 15:07:20 -0600

    tweet_post

--- main.py
+++ main.py
@@ -189,11 +189,38 @@ def home():
     path="/post",
     response_model=Tweet,
     status_code=status.HTTP_201_CREATED,
-    summary="Post a tweer",
+    summary="Post a tweet",
     tags=["Tweets"]
 )
-def post():
-    pass
+def post(tweet: Tweet = Body(...)):
+    """
+    Post a Tweet
+
+    This path operation post a tweet in the app
+
+    Parameters:
+    - Request body parameter
+        - **tweet: Tweet**
+
+    Returns a json with the basic tweet information:
+    - tweet_id: UUID
+    - content: str
+    - created_at: datetime
+    - update_at: Optional[datetime]
+    - by: User
+    """
+    with open("tweets.json", "r+", encoding="utf-8") as f:
+        results = json.loads(f.read())
+        tweet_dict = tweet.dict()
+        tweet_dict["tweet_id"] = str(tweet_dict["tweet_id"])
+        tweet_dict["created_at"] = str(tweet_dict["created_at"])
+        tweet_dict["update_at"] = str(tweet_dict["update_at"])
+        tweet_dict["by"]["user_id"] = str(tweet_dict["by"]["user_id"])
+        tweet_dict["by"]["birth_date"] = str(tweet_dict["by"]["birth_date"])
+        results.append(tweet_dict)
+        f.seek(0)
+        f.write(json.dumps(results))
+        return tweet
 
 ### Show a tweet
 @app.get(
